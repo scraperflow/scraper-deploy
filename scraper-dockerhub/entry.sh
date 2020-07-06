8a2e7f4ace82f253b172fb18ab24b3a03d9d3e31
Albert Schimpf <git@schimpf.link>
2020-07-06 16:46:21 +0200

    Docker

--- scraper-dockerhub/entry.sh
+++ scraper-dockerhub/entry.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
 
-# compile nodes with api and annotations
-mkdir -p /tmp/nodes/scraper
-mkdir -p /nodes
-javac /custom/*.java -cp /bin/scraper.jar
-
-# put them into the 'scraper' package for discovery
-mv /custom/*.class /tmp/nodes/scraper
-jar cvf /tmp/nodes/nodes.jar -C /tmp/nodes .
-rm -rf /tmp/nodes/scraper
-
-# move into discoverable location
-mv /tmp/nodes/nodes.jar /nodes/
-
 # execute scraper
-java -cp /bin/scraper.jar:/nodes/*:/plugins/*:/addons/* scraper.app.Scraper
+java -p /bin:/nodes:/plugins:/addons --add-modules ALL-MODULE-PATH -m scraper.app "$@"
